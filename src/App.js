bae822fef7b7ee314d67ef5811da6556689a8bf0
smenard215 <s-menard@live.com>
2024-07-30 14:47:33 -0400

    added header, footer, main and nav components. Updated app js file, added index html file

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import './App.css';
+import Header from './Components/Header';
+import Nav from './Components/Nav';
+import Main from './Components/Main';
+import Footer from './Components/Footer';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <Header />
+     <Nav />
+     <Main />
+     <Footer />
+    </>
   );
 }
 
---
+++ src/Components/Nav.js
@@ -0,0 +1,18 @@
+function Nav() {
+    return(
+        <nav>
+            <img src='./Assets/Logo.svg' alt="Logo" />
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Menu</a></li>
+                <li><a href="#">Reservations</a></li>
+                <li><a href="#">Order Online</a></li>
+                <li><a href="#">Login</a></li>
+            </ul>
+
+        </nav>
+    );
+}
+
+export default Nav();
